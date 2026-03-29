1e51825f279fd604680fdef6b0a91a7e2a6c549e
mutesupply <mutesupply24@gmail.com>
2026-03-29 13:22:11 +0700

    fix dropdown bluetooth

--- lib/main.dart
+++ lib/main.dart
@@ -840,17 +840,19 @@ Container(
 
     icon: const Icon(Icons.bluetooth, color: Colors.cyanAccent),
 
-    items: devicesList.map((device) {
-      return DropdownMenuItem(
-        value: device,
-String name = deviceNames[device.id.toString()] ??
-              device.platformName;
+items: devicesList.map((device) {
 
-child: Text(
-  name.isNotEmpty ? name : "ESP (${device.id})",
-),
-      );
-    }).toList(),
+  String name = deviceNames[device.id.toString()] ??
+                device.platformName;
+
+  return DropdownMenuItem<BluetoothDevice>(
+    value: device,
+    child: Text(
+      name.isNotEmpty ? name : "ESP (${device.id})",
+    ),
+  );
+
+}).toList(),
 
 onChanged: (device) async {
   setState(() {
